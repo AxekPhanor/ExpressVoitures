b00a13028cd7a1fd2ecc84d9d492946310ee244e
Axek <axel.phanor64@gmail.com>
2024-02-21 11:09:30 +0100

    init: Création du projet

---
+++ ExpressVoitures.Server/Data/ExpressVoituresDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpressVoitures.Server.Data
+{
+    public class ExpressVoituresDbContext : IdentityDbContext
+    {
+        public ExpressVoituresDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
